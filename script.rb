c8b8949ec395c8ffffa52ceaaf41284dbfb6a593
Julius <123553495+Julius0087@users.noreply.github.com>
2023-07-17 11:48:14 +0200

    Version 0.2
    - add 12 round game cycle
    - add input handling
    - add game functionality with proper feedback
    - add private protection for code variable in Computer class

--- script.rb
+++ script.rb
@@ -1,6 +1,36 @@
-color_array = ["R", "G", "B", "Y", "W", "P"]
+COLOR_ARRAY = ["R", "G", "B", "Y", "W", "P"]
 
 class Computer
+
+  @code = ''
+
+  def set_code
+    @code = generate_code(COLOR_ARRAY).join
+  end
+
+  def give_feedback(guess)
+    guess = guess.split('')
+    code_array = @code.split('')
+    red_feedback = 0
+    red_feedback_array = []
+    guess.each_with_index do |color, index|
+      if code_array[index] == color
+        red_feedback += 1
+        red_feedback_array.push(color)
+      end
+    end
+
+    white_feedback = 0
+    remaining_array = guess.difference(red_feedback_array)
+    remaining_array.each do |color|
+      white_feedback += 1 if code_array.include?(color)
+    end
+    "You guessed #{red_feedback} colors at the correct position and #{white_feedback} correct colors at the wrong position."
+  end
+
+  private
+
+  attr_accessor :code
 
   def generate_code(color_array)
     code_array = []
@@ -13,31 +43,45 @@ class Computer
     code_array
   end
 
-  def give_feedback(guess, code_array)
-    red_feedback = 0
-    red_feedback_array = []
-    guess.each_with_index do |color, index|
-      if code_array[index] == color
-        red_feedback += 1 
-        red_feedback_array.push(color)
-      end
-    end
-
-    white_feedback = 0
-    remaining_array = guess.difference(red_feedback_array)
-    remaining_array.each do |color| 
-      white_feedback += 1 if code_array.include?(color)
-    end
-    "You guessed #{red_feedback} colors at the correct position and #{white_feedback} correct colors at the wrong position."
-  end
-
 end
 
+class Player
+
+  def guess(string)
+    string.upcase.split('')
+  end
+end
+
+def check_validity(array)
+  array.each do |item|
+    unless COLOR_ARRAY.include?(item)
+      return false
+    end
+    true
+  end
+end
+
+player = Player.new
 computer = Computer.new
-code_array = computer.generate_code(color_array)
-p code_array
-guess = ["R", "Y", "B", "G"]
-puts computer.give_feedback(guess, code_array)
+code_array = computer.set_code.split('')
+
+for i in 1..12
+  puts "Round #{i}. Guess the colors."
+  guess = ''
+  loop do
+    guess = gets.chomp.upcase
+    if guess.match?(/[A-Z]/) && guess.length == 4 && check_validity(guess.split(''))
+      break
+    else
+      puts 'Invalid guess. Select only four of color letters (R, G, B, Y, W, P)'
+    end
+  end
+
+  guess_array = player.guess(guess)
+  puts computer.give_feedback(guess)
+
+end
+puts code_array.split(' ')
 
 # TODO:
 # make sure code isn't accessible, only inside the computer class (try making it a class variable @@)
